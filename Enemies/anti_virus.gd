a0611b1486c48b031b2db5be14d43ddd3d221f03
Xentira <bensnyder1@gmail.com>
2024-01-16 11:44:03 -0500

    Corrections
    Fixed the animations for the idle, it was jerky needed to add additional time.
    
    Switched to using ENUMS for the states to make code more readable
    
    Tweeks to player to make adjustments for scripts to work when its dropped into a new scene.
    
    Fixed tile size on the meshlib

--- Enemies/anti_virus.gd
+++ Enemies/anti_virus.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-
+enum States {IDLE = 0, MOVING = 1, DEAD = 2, MELEE = 3}
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -11,26 +11,25 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var player: CharacterBody3D = $"../Player" # Getting the player
-
-func _process(delta) -> void:
-	animation_player.speed_scale = speedMove / 2
-	if state == 1:
-		var movement_position = player.transform.origin # Set the position to look at
-		var transform_val = self.transform.looking_at(movement_position, Vector3.UP) # Set the transform value
-		self.transform  = self.transform.interpolate_with(transform_val, gravSpeed * delta) # making the object look at the player
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	rotation.x = 0
-	if state == 1:
+	if state == States.MOVING:
+		animation_player.speed_scale = speedMove / 2
+		var movement_position = player.transform.origin # Set the position to look at
+		var transform_val = self.transform.looking_at(movement_position, Vector3.UP) # Set the transform value
+		self.transform  = self.transform.interpolate_with(transform_val, gravSpeed * delta) # making the object look at the player
 		var direction = (player.position - self.position).normalized()
 		velocity.x = direction.x * speedMove
 		velocity.z = direction.z * speedMove
 		
 		animation_player.play("Walk Cycle") # play the animation
 		
-	if state == 0:
+	if state == States.IDLE:
+		#reset to default speed for animation
+		animation_player.speed_scale = 1
 		animation_player.play("Idle") # play the animation
 	move_and_slide()
