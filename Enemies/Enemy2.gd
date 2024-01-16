ffceecc455d499b705b45a64e4c2d623a0dc636b
BananaCreeper08 <dustywsnyder@gmail.com>
2024-01-15 20:39:26 -0500

    Trying to make Antivirus
    Its not working correctly and floating above the ground

--- Enemies/Enemy2.gd
+++ Enemies/Enemy2.gd
@@ -12,4 +12,4 @@ func _physics_process(delta: float) -> void:
 func _process(delta) -> void:
 	var movement_position = player.transform.origin # Set the position to look at
 	var transform_val = self.transform.looking_at(movement_position, Vector3.UP) # Set the transform value
-	self.transform  = self.transform.interpolate_with(transform_val, speed * delta) # making the object look at the player
+	self.transform  = self.transform.interpolate_with(transform_val, speed * delta) # making the object look at the player 
