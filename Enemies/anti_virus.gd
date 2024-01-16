7864dd725beda59ea9dbd23a6bc4987739fb9d7a
BananaCreeper08 <dustywsnyder@gmail.com>
2024-01-16 11:11:19 -0500

    Speed Change
    You can now change the speed of the Anti-Virus

--- Enemies/anti_virus.gd
+++ Enemies/anti_virus.gd
@@ -1,31 +1,36 @@
 extends CharacterBody3D
 
 
-const speedMove = 1
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export var state = 0 # 0 is idle 1 is moving.
+@export var speedMove = 3.5 # The moving speed.
+@export var gravSpeed = 5 # Set the speed value.
 
-#@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var player: CharacterBody3D = $"../Player" # Getting the player
-var speed = 5 # Set the speed value
-var state = 0 # 0 is idle 1 is moving
 
 func _process(delta) -> void:
-	var movement_position = player.transform.origin # Set the position to look at
-	var transform_val = self.transform.looking_at(movement_position, Vector3.UP) # Set the transform value
-	self.transform  = self.transform.interpolate_with(transform_val, speed * delta) # making the object look at the player
+	animation_player.speed_scale = speedMove / 2
+	if state == 1:
+		var movement_position = player.transform.origin # Set the position to look at
+		var transform_val = self.transform.looking_at(movement_position, Vector3.UP) # Set the transform value
+		self.transform  = self.transform.interpolate_with(transform_val, gravSpeed * delta) # making the object look at the player
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	rotation.x = 0
-	if state == 0:
+	if state == 1:
 		var direction = (player.position - self.position).normalized()
 		velocity.x = direction.x * speedMove
-		velocity.z = direction.z * speedMove	
-		#pass
+		velocity.z = direction.z * speedMove
+		
+		animation_player.play("Walk Cycle") # play the animation
+		
+	if state == 0:
+		animation_player.play("Idle") # play the animation
 	move_and_slide()
-	#animation_player.play("Walk Cycle")
