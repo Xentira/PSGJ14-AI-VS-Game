64ba7a6881a3a25f1a742a98105ed8b1e85fe269
BananaCreeper08 <dustywsnyder@gmail.com>
2024-01-26 18:32:41 -0500

    Particles

---
+++ Pivot.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@export var rotationSpeed = 5
+
+var rotationAmmount = 0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotationAmmount += rotationSpeed * delta
+	rotation.y = rotationAmmount
