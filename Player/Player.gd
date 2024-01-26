e4db6e1464db72b71e0af3c5ec747c3255cb9d97
Xentira <bensnyder1@gmail.com>
2024-01-26 17:39:02 -0500

    Attempting fix of map and tile error from last push

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,8 @@ signal WormAttack(damage:int)
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+@onready var camera: Camera3D = $Camera
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
