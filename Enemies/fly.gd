e27a9eb23a4d78b5a7698910af214ad1a25d966a
BananaCreeper08 <dustywsnyder@gmail.com>
2024-02-01 17:51:52 -0500

    Fly can now be killed

--- Enemies/fly.gd
+++ Enemies/fly.gd
@@ -9,17 +9,37 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export var speedMove = 3.5 # The moving speed.
 @export var gravSpeed = 5 # Set the speed value.
 @export var isGrounded = false
-@export var justSpawned = false;
+@export var justSpawned = false
 @export var spawnDelay = 4.0
+@export var isDead = false
+@export var isDeadAnimationPlayed = false
 
+@export var active: bool = true
+@export var health: int = 100
+@export var shield: int = 0
+@export var damage: int = 10
 #@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var player: CharacterBody3D = $"../Player" # Getting the player
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var spotting_area: Area3D = $SpottingArea
 @onready var gameManager = $"../GameManager"
+@onready var health_bar: ProgressBar = $HealthBar/SubViewport2/HealthBar
+@onready var shield_bar: ProgressBar = $HealthBar/SubViewport2/ShieldBar
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
 
 func _ready() -> void:
-	pass
+	health_bar.max_value = health
+	health_bar.value = health
+	shield_bar.max_value = shield
+	shield_bar.value = shield
+	if shield <= 0:
+		shield_bar.visible = false
+	player.WormAttack.connect(changeHealth)
+	if health_bar != null:
+		health_bar.value = health
+	else:
+		print("healthbar error , " + str(health_bar))
+	
 
 func _physics_process(delta: float) -> void:
 	if isGrounded:
@@ -27,20 +47,33 @@ func _physics_process(delta: float) -> void:
 	
 	match state:
 		States.MOVING:
-			animation_player.speed_scale = speedMove / 2
-			animation_player.play("Walk Cycle") # play the animation
-			var movement_position = player.transform.origin # Set the position to look at
-			var transform_val = self.transform.looking_at(movement_position, Vector3.UP) # Set the transform value
-			self.transform  = self.transform.interpolate_with(transform_val, gravSpeed * delta) # making the object look at the player
-			var direction = (player.position - self.position).normalized()
-			velocity = direction * speedMove
+			if !isDead:
+				animation_player.speed_scale = speedMove / 2
+				animation_player.play("Walk Cycle") # play the animation
+				var movement_position = player.transform.origin # Set the position to look at
+				var transform_val = self.transform.looking_at(movement_position, Vector3.UP) # Set the transform value
+				self.transform  = self.transform.interpolate_with(transform_val, gravSpeed * delta) # making the object look at the player
+				var direction = (player.position - self.position).normalized()
+				velocity = direction * speedMove
 		States.DEAD:
+			isDead = true
 			if not isGrounded:
-				animation_player.play("Dying") # play the animation
-			isGrounded = true
+				if not isDeadAnimationPlayed:
+					animation_player.play("Dying") # play the animation
+					isDeadAnimationPlayed = true 
+					isGrounded = true
+			else:
+				pass
+				if not isDeadAnimationPlayed:
+					animation_player.play("Dying") # play the animation
+					isDeadAnimationPlayed = true 
+			
 			#reset to default speed for animation
 			#animation_player.speed_scale = 1
-			animation_player.play("Dying") # play the animation
+			
+			collision_shape_3d.disabled = true
+			await get_tree().create_timer(2.5).timeout
+			queue_free()
 			if not is_on_floor():
 				velocity.y -= gravity * delta
 		States.IDLE:
@@ -57,22 +90,38 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _on_spotting_area_body_entered(body: Node3D) -> void:
-	if body == player:
-		if state == States.IDLE or state == States.MOVING:
-			state = States.SPOTTED
-		var timer = Timer.new()
-		add_child(timer)
-		timer.wait_time = spawnDelay
-		timer.connect("timeout", allowSpawning)
-		if(body == player) and not justSpawned:
-			gameManager.spawnAntiVirusGroup(5, self.position, 30)
-			justSpawned = true
-			timer.start()
+	if !isDead:
+		if body == player:
+			if state == States.IDLE or state == States.MOVING:
+				state = States.SPOTTED
+			var timer = Timer.new()
+			add_child(timer)
+			timer.wait_time = spawnDelay
+			timer.connect("timeout", allowSpawning)
+			if(body == player) and not justSpawned:
+				gameManager.spawnAntiVirusGroup(5, self.position, 30)
+				justSpawned = true
+				timer.start()
 
 func _on_spotting_area_body_exited(body: Node3D) -> void:
-	if body == player:
-		state = States.MOVING
+	if !isDead:
+		if body == player:
+			state = States.MOVING
+		
+	
 
 func allowSpawning() -> void:
 	justSpawned = false
 	#print("spawing renabled")
+
+func changeHealth(amount: int) -> void:
+	if shield_bar.visible == true:
+		shield += amount
+		shield_bar.value = shield
+		if shield <= 0:
+			shield_bar.visible = false
+	else:
+		health += amount
+		health_bar.value = health
+		if health <= 0:
+			state = States.DEAD
